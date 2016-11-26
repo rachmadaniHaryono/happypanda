9c2d73f8f6e406df5f3cc66c6789019a12d6f255
rachmadaniHaryono <foreturiga@gmail.com>
2016-11-26 09:52:15 +0800

    convert classmethod _set_gallery_specific_setting into function.

--- tests/test_gallery_dialog_widget.py
+++ tests/test_gallery_dialog_widget.py
@@ -24,27 +24,25 @@ def test_set_gallery_specific_setting(val_pack, find_combobox_match_result, qtbo
     m_g = mock.Mock()
     setattr(m_g, attr, attr_val)
     gallery = [m_g]
-    m_cls_attr_val = mock.Mock()
-    with mock.patch(
-            'version.gallery_dialog_widget.GalleryDialogWidget._find_combobox_match',
-            return_value=find_combobox_match_result
-    ) as m_find_combobox_match:
-        from version.gallery_dialog_widget import GalleryDialogWidget
-        from version.gallery_dialog_widget import app_constants
-        setattr(GalleryDialogWidget, cls_attr, m_cls_attr_val)
-        # run
-        GalleryDialogWidget._set_gallery_specific_setting(
-            attr=attr, cls_attr=cls_attr, gallery=gallery, def_val=def_val,
-            constant_attr=constant_attr
-        )
-        # test
-        # _find_combobox_match function first call and second call
-        fcm_func_1call = m_cls_attr_val, getattr(gallery[0], attr), def_val
-        fcm_func_2call = m_cls_attr_val, getattr(app_constants, constant_attr), def_val
-        if find_combobox_match_result:
-            m_find_combobox_match.assert_called_once_with(*fcm_func_1call)
-        else:
-            m_find_combobox_match.assert_has_calls([
-                mock.call(*fcm_func_1call),
-                mock.call(*fcm_func_2call)
-            ])
+    m_cls = mock.Mock()
+    from version.gallery_dialog_widget import _set_gallery_specific_setting
+    from version.gallery_dialog_widget import app_constants
+    m_cls._find_combobox_match.return_value = find_combobox_match_result
+    # run
+    _set_gallery_specific_setting(
+        cls=m_cls,
+        attr=attr, cls_attr=cls_attr, gallery=gallery, def_val=def_val,
+        constant_attr=constant_attr
+    )
+    # test
+    # _find_combobox_match function first call and second call
+    getattr(m_cls, cls_attr).g_check.setChecked.assert_called_once_with(True)
+    fcm_func_1call = getattr(m_cls, cls_attr), getattr(gallery[0], attr), def_val
+    fcm_func_2call = getattr(m_cls, cls_attr), getattr(app_constants, constant_attr), def_val
+    if find_combobox_match_result:
+        m_cls._find_combobox_match.assert_called_once_with(*fcm_func_1call)
+    else:
+        m_cls._find_combobox_match.assert_has_calls([
+            mock.call(*fcm_func_1call),
+            mock.call(*fcm_func_2call)
+        ])
