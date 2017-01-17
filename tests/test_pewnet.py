3309098b7102ba1fb7fd7a7b13edb39652bf8d30
rachmadaniHaryono <foreturiga@gmail.com>
2017-01-17 15:18:43 +0800

    chg: test: update test for pewnet.

--- tests/test_pewnet.py
+++ tests/test_pewnet.py
@@ -13,7 +13,8 @@ import pytest
             'https://panda.chaika.moe/archive/19970/',
             'http://g.e-hentai.org/g/1003511/5f82a225fb/',
             "http://asmhentai.com/g/168735/",
-            'http://example.com'
+            'http://example.com',
+            'https://nhentai.net/g/184905/',
         ],
         [True, False]
     )
@@ -24,6 +25,7 @@ def test_website_validator(url, check_result):
             mock.patch('version.pewnet.ExHenManager') as m_ehm, \
             mock.patch('version.pewnet.ChaikaManager') as m_cm, \
             mock.patch('version.pewnet.AsmManager') as m_am, \
+            mock.patch('version.pewnet.NhenManager') as m_nm, \
             mock.patch('version.pewnet.settings') as m_settings:
         from version.pewnet import website_validator
         from version import app_constants
@@ -32,7 +34,8 @@ def test_website_validator(url, check_result):
             ('g.e-hentai', m_hm.return_value),
             ('exhentai', m_ehm.return_value),
             ('panda.chaika.moe', m_cm.return_value),
-            ('asmhentai', m_am.return_value)
+            ('asmhentai', m_am.return_value),
+            ('nhentai.net', m_nm.return_value),
         ]
         # test
         if not all(x[0] in url for x in exp_res_packs):
