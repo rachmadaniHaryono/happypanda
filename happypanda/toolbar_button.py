6ab1be0f7675d4be77d24ee199fc1b8632c1be1c
rachmadaniHaryono <foreturiga@gmail.com>
2017-02-17 12:29:56 +0800

    chg: dev: update based on package branch

--- happypanda/toolbar_button.py
+++ happypanda/toolbar_button.py
@@ -1,4 +1,7 @@
-"""toolbar button."""
+"""toolbar button.
+
+taken from misc.py
+"""
 from PyQt5.QtCore import (
     pyqtSignal,
     QRectF,
@@ -74,59 +77,3 @@ class ToolbarButton(QPushButton):
             event.accept()
         else:
             event.ignore()
-
-    def paintEvent(self, event):  # NOQA
-        """Paint event.
-
-        Args:
-            event (QtGui.QPaintEvent): Paint event.
-        """
-        assert isinstance(event, QPaintEvent)
-        painter = QPainter(self)
-        opt = QStyleOption()
-        opt.initFrom(self)
-        # self.style().drawPrimitive(self.style().PE_FrameButtonTool, opt, painter, self)
-
-        # painter.setPen(QColor(164,164,164,120))
-        # painter.setBrush(Qt.NoBrush)
-        if self._selected:
-            painter.setPen(QColor("#d64933"))
-        # painter.setPen(Qt.NoPen)
-        painter.setRenderHint(painter.Antialiasing)
-        ch_width = self._font_metrics.averageCharWidth() / 4
-        ch_height = self._font_metrics.height()
-        but_rect = QRectF(
-            ch_width, ch_width, self.width() - ch_width * 2, self.height() - ch_width * 2)
-        select_rect = QRectF(0, 0, self.width(), self.height())
-
-        painter.drawRoundedRect(but_rect, ch_width, ch_width)
-        txt_to_draw = self._font_metrics.elidedText(
-            self._text, Qt.ElideRight, but_rect.width())
-
-        but_center = (but_rect.height() - ch_height) / 2
-        text_rect = QRectF(
-            but_rect.x() + ch_width * 2,
-            but_rect.y() + but_center, but_rect.width(), but_rect.height())
-        # painter.setPen(QColor('white'))
-        painter.drawText(text_rect, txt_to_draw)
-
-        if self.underMouse():
-            painter.save()
-            painter.setPen(Qt.NoPen)
-            painter.setBrush(QBrush(QColor(164, 164, 164, 90)))
-            painter.drawRoundedRect(select_rect, 2, 2)
-            painter.restore()
-
-    def setText(self, txt):  # NOQA
-        """set Text.
-
-        Args:
-            txt: Text.
-        """
-        self._text = txt
-        self.update()
-        super().setText(txt)
-
-    def text(self):
-        """Text."""
-        return self._text
