a5e239851d048adbc33f7c6819514ce106c0cbcb
rachmadaniHaryono <foreturiga@gmail.com>
2017-05-24 19:54:34 +0800

    chg: dev: update init
    - new metadata (__author_name__, __app_name__)
    - change author metadata, split the name and the email address

--- happypanda/__init__.py
+++ happypanda/__init__.py
@@ -1,4 +1,6 @@
 """happypanda - A manga/doujinshi manager with tagging support."""
 __version__ = '1.1.1'
-__author__ = 'Pewpews <pew@pewpew.moe>'
+__author_name__ = 'Pewpews'
+__author__ = '{} <pew@pewpew.moe>'.format(__author_name__)
 __all__ = []
+__app_name__ = 'Happypanda'
