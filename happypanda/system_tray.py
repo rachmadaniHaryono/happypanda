eccc97ffcf6b07396b1b68760d7d133cf7386da5
rachmadaniHaryono <foreturiga@gmail.com>
2017-05-26 14:42:42 +0800

    chg: dev: remove linux limit on system_tray

--- happypanda/system_tray.py
+++ happypanda/system_tray.py
@@ -1,11 +1,6 @@
 """system tray."""
 from PyQt5.QtWidgets import QSystemTrayIcon
 
-try:
-    import app_constants
-except ImportError:
-    from . import app_constants
-
 
 class SystemTray(QSystemTrayIcon):
     """SystemTray.
@@ -26,8 +21,8 @@ class SystemTray(QSystemTrayIcon):
         super().__init__(icon, parent=None)
         self.parent_widget = parent
 
-    def showMessage(self, title, msg, icon=QSystemTrayIcon.Information,
-                    msecs=10000, minimized=False):
+    def showMessage(
+            self, title, msg, icon=QSystemTrayIcon.Information, msecs=10000, minimized=False):
         """show message.
 
         Args:
@@ -37,11 +32,8 @@ class SystemTray(QSystemTrayIcon):
             msecs (int): Duration to show message in milliseconds
             minimized (bool): Minimize.
         """
-        # NOTE: Crashes on linux
-        # TODO: Fix this!!
-        if not app_constants.OS_NAME == "linux":
-            if minimized:
-                if self.parent_widget.isMinimized() or not self.parent_widget.isActiveWindow():
-                    return super().showMessage(title, msg, icon, msecs)
-            else:
+        if minimized:
+            if self.parent_widget.isMinimized() or not self.parent_widget.isActiveWindow():
                 return super().showMessage(title, msg, icon, msecs)
+        else:
+            return super().showMessage(title, msg, icon, msecs)
