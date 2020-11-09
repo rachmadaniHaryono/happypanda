8c1da517ca5e19b578bb49134108dabd44249215
rabaarabaa <27743418+rabaarabaa@users.noreply.github.com>
2020-11-09 01:12:55 -0300

    executors.py: Temp import fix.

--- version/executors.py
+++ version/executors.py
@@ -4,9 +4,14 @@ from concurrent import futures
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QImage, QPainter, QBrush, QPen
 
-from database import db_constants
-import utils
-import app_constants
+try:
+	from database import db_constants
+	import utils
+	import app_constants
+except ImportError:
+	from .database import db_constants
+	from . import utils
+	from . import app_constants
 
 log = logging.getLogger(__name__)
 log_i = log.info
